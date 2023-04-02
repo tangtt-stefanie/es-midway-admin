81ad60eb78399f29a35ffee77898fe2678533633
tangtt <1329562389@qq.com>
2023-04-02 16:02:22 +0800

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,5 @@ module.exports = {
   trailingComma: 'none', // 去除对象最末尾元素跟随的逗号
   bracketSpacing: true, // 是否在对象属性添加空格
   semi: false, // 是否在语句末尾打印分号
+  endOfLine: 'auto' // 解决Delete `␍`eslintprettier/prettier
 }
